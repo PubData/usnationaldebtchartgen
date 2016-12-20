71a49766eb43f4c30c190f10380ac1a9bfc82be7
Stephen <sgornick@digicoast.com>
2016-12-20 03:49:49 -0800

    Refactor module.

--- usnationaldebtchartgen.py
+++ usnationaldebtchartgen.py
@@ -16,83 +16,122 @@ app = Flask(__name__)
 @app.route('/chart/<filename>.<file_ext>', methods=['GET'])
 @app.route('/chart/<filename>', methods=['GET'])
 def chart(filename='chart', file_ext='png', width_str=None):
-    output_types = {
-        'png': 'image/png',
-        'jpg': 'image/jpeg',
-        'jpeg': 'image/jpeg',
-        'pdf': 'application/pdf',
-        'svg': 'image/svg+xml',
-        'svg+xml': 'image/svg+xml',
-        'xml': 'image/svg+xml',
-    }
-    
-    if file_ext and file_ext.lower() not in output_types:
-        abort(404)
-    
-    req = request.Request(url='https://treasurydirect.gov/NP/debt/current')
+	url="http://export.highcharts.com"
+	output_types = {
+		'png': 'image/png',
+		'jpg': 'image/jpeg',
+		'jpeg': 'image/jpeg',
+		'pdf': 'application/pdf',
+		'svg': 'image/svg+xml',
+		'svg+xml': 'image/svg+xml',
+		'xml': 'image/svg+xml',
+	}
+	
+	if file_ext and file_ext.lower() not in output_types:
+		abort(404)
+	
+	# Read infile from filesystem into a variable.
+	with app.open_resource('data/infile.js', 'r') as f:        
+		infile = f.read()    
 
-    with request.urlopen(req) as response:
-        page = response.read()
+	end_date = scrape_effective_date()
+	start_date = (end_date - timedelta(days=365))
+	
+	# Prepare the post_data
+	h = {'Content-Type':'application/json', 'User-Agent': 'curl'}
+	post_data = {}
+	post_data['infile'] = \
+		'{}data: [\n{}\n{}'.format(
+			infile.split('data: [\n')[0],
+			render_to_infile(scrape_history_data(start_date, end_date)),
+			infile.split('data: [\n')[1])
 
-    tree = html.fromstring(page)
-    end_date_str = tree.xpath('//table[@class="data1"]/tr/td[1]/text()')[0]
-    end_date = datetime.strptime(end_date_str, '%m/%d/%Y')
-    start_date = (end_date - timedelta(days=365))
+	if file_ext and file_ext != 'png':
+		post_data['type'] = output_types.get(file_ext.lower(), 'image/png')
+	if (post_data.get('type') == 'image/svg+xml' and \
+			flask_request.args.get('scale')):
+		post_data['scale'] = flask_request.args.get('scale')
+	if width_str and int(width_str) > 0:
+		post_data['width'] = int(width_str)
+		
+	post_data_json = json.dumps(post_data).encode('utf-8')
+	
+	req = request.Request(url=url, data=post_data_json, headers=h)
+	
+	with request.urlopen(req) as response:
+		mimetype = response.headers['Content-Type']
+		image_data = io.BytesIO(response.read())
 
-    req = request.Request(
-        url="https://treasurydirect.gov/NP/debt/search?startMonth={}&startDay={}&startYear={}&endMonth={}&endDay={}&endYear={}".format(
-            start_date.month,
-            start_date.day,
-            start_date.year,
-            end_date.month,
-            end_date.day,
-            end_date.year))
+	return send_file(
+		image_data,
+		attachment_filename=secure_filename('{}.{}'.format(filename, file_ext)),
+		mimetype=mimetype)
 
-    with request.urlopen(req) as response:
-        page = response.read()
+def scrape_effective_date():
+	# Scrape the "current" (effective) date from the U.S. Treasury site.
+	url = 'https://treasurydirect.gov/NP/debt/current'
+	
+	req = request.Request(url=url)
 
-    tree = html.fromstring(page)
-    records = ''
+	with request.urlopen(req) as response:
+		page = response.read()
 
-    for row in tree.xpath('//table[@class="data1"]/tr[position()>1]'):
-        date_str = row.xpath('./td')[0].xpath('text()')[0]
-        amount_str = row.xpath('./td')[3].xpath('text()')[0]
-        date = datetime.strptime(date_str, '%m/%d/%Y')
-        # Add row of data as:
-        # [Date.UTC(yyyy, mm, dd), amount],
-        records += (
-            '[{}, {}],\n'.format(
-                'Date.UTC({}, {}, {})'.format(date.year, date.month - 1, date.day),
-                amount_str.replace(',', '')));
+	tree = html.fromstring(page)
+	date_str = tree.xpath('//table[@class="data1"]/tr/td[1]/text()')[0]
+	return datetime.strptime(date_str, '%m/%d/%Y')
 
-    records = '{}\n'.format(records[:-2])  # On the last line, no comma.
 
-    with app.open_resource('data/infile.js', 'r') as f:
-        infile = f.read()
+def scrape_history_data(start_date, end_date):
+	# Scrape history data from the search results of the US Treasury site.
+	url = (
+		"https://treasurydirect.gov/NP/debt/search?" +
+		"startMonth={}&startDay={}&startYear={}" + 
+		"&endMonth={}&endDay={}&endYear={}")
+		
+	url = url.format(
+			start_date.month,
+			start_date.day,
+			start_date.year,
+			end_date.month,
+			end_date.day,
+			end_date.year)
+	
+	req = request.Request(url=url)
 
-    url="http://export.highcharts.com"
-    h = {'Content-Type':'application/json', 'User-Agent': 'curl'}
-    post_data = {}
-    post_data['infile'] = \
-        '{}data: [\n{}\n{}'.format(
-            infile.split('data: [\n')[0],
-            records,
-            infile.split('data: [\n')[1])
+	with request.urlopen(req) as response:
+		page = response.read()
 
-    if file_ext and file_ext != 'png':
-        post_data['type'] = output_types.get(file_ext.lower(), 'image/png')
-    if (post_data.get('type') == 'image/svg+xml' and \
-            flask_request.args.get('scale')):
-        post_data['scale'] = flask_request.args.get('scale')
-    if width_str and int(width_str) > 0:
-        post_data['width'] = int(width_str)
-        
-    data = json.dumps(post_data).encode('utf-8')
-    req = request.Request(url=url, data=data, headers=h)
-    fd = request.urlopen(req)
-    image_data = io.BytesIO(fd.read())
+	tree = html.fromstring(page)
+	history_data = []
 
-    return send_file(
-        image_data,
-        attachment_filename=secure_filename('{}.{}'.format(filename, file_ext)),
-        mimetype=fd.headers['Content-Type'])
+	for row in tree.xpath('//table[@class="data1"]/tr[position()>1]'):
+		date_str = row.xpath('./td')[0].xpath('text()')[0]
+		date = datetime.strptime(date_str, '%m/%d/%Y')
+		amount_str = row.xpath('./td')[3].xpath('text()')[0]
+		amount = float(amount_str.replace(',', ''))
+		
+		history_data.append({'date': date, 'amount': amount})
+		
+	return history_data
+				
+
+def render_to_infile(history_data):
+	# Produce a string containing the history data ready for insert into the  
+	# infile that gets passed to the Highcharts export server.
+	output = '' 
+
+	for row in history_data:
+		# Add row of data as:
+		# [Date.UTC(yyyy, mm, dd), amount],
+		date_str = 'Date.UTC({}, {}, {})'.format(
+			row['date'].year, row['date'].month - 1, row['date'].day)
+		
+		output += '[{}, {:.2f}],\n'.format(date_str, row['amount'])
+			
+	output = '{}'.format(output[:-2])  # On the last line, no comma.
+	
+	if output:
+		# Add newline on last line.
+		output += '\n'
+
+	return output
